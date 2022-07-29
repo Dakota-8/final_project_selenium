4c9e171df171f4c27df576600b4978ace12ca4d8
Dakota-8 <fr0jkeeez@gmail.com>
2022-07-29 09:34:48 +0600

    changed selector to valid

--- pages/main_page.py
+++ pages/main_page.py
@@ -8,4 +8,4 @@ class MainPage(BasePage):
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
