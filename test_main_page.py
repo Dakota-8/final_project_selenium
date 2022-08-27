49b6e78754c6e723eeb433ec02f40e945fc6b8ed
Dakota-8 <fr0jkeeez@gmail.com>
2022-08-27 10:53:13 +0600

    changed import from '.pages' to 'pages'

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,6 @@
-from .pages.main_page import MainPage
-from .pages.login_page import LoginPage
-from .pages.basket_page import BasketPage
+from pages.main_page import MainPage
+from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
 import pytest
 
 
